ce76da2ffb23a788cf9ac32097d3b0b975a43054
Alex Salam <vornat2@gmail.com>
2018-06-05 21:48:49 +0100

    wrapped domain in quites

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server subastral.net, user: "deployer", roles: %w{app}
+server "subastral.net", user: "deployer", roles: %w{app}
 
 append :linked_files, ".env.production"
 
