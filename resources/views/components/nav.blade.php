c2a2b4c22303e320140db5777f870c19bd374eff
Alex Salam <a.salam@cobwebinfo.com>
2018-06-06 17:20:52 +0100

    initial homepage

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,9 @@
+<div class="logo-title-container">
+    <img class="logo" src="http://via.placeholder.com/75x75" />
+</div>
+
+<h1 class="title">Subastral</h1>
+
+<div class="pull-right login-form">
+    <p>LOG IN</p>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,22 @@
+@extends('layouts.app')
 
+@section('content')
+
+    <div class="row">
+        <div class="col-md-6">
+            <p>1</p>
+        </div>
+        <div class="col-md-6">
+            <p>2</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <p>3</p>
+        </div>
+        <div class="col-md-6">
+            <p>4</p>
+        </div>
+    </div>
+
+@endsection
