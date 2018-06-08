4a9df1732958d1e30cd5fcf4e5ebb27f4a9479b1
Alex Salam <a.salam@cobwebinfo.com>
2018-06-08 11:57:39 +0100

    some homepage content and campaign selector form

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,14 @@
 <div class="sidebar">
     <p>SIDEBAR</p>
     <div class="campaign-selector">
-
+        <h4 class="title">Select your campaign</h4>
+        <form action="/campaign/set" method="post" id="campaign-selector">
+            <div class="form-group">
+                <select id="campaign-select" form="campaign-selector" class="form-control" name="campaign">
+                    <option value="test">Test Campaign</option>
+                </select>
+            </div>
+        </form>
     </div>
     <nav>
         <a href="/about">About</a>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,33 +4,31 @@
 
     <div style="margin-top:30px" class="row">
         <div class="col-md-6">
-            <div class="panel panel-default top-left">
+            <div class="panel panel-default top-left home">
                 <div class="panel-heading">
-                    <h3>This site is awesome!</h3>
+                    <h3>What is Subastral?</h3>
                 </div>
                 <div class="panel-body">
-                    <ul>
-                        <li>Store monsters, npcs, places and more!</li>
-                    </ul>
+                    <p>Subastral is a campaign management service to allow dungeon masters to easily keep track of all the content they have created over the years and organise it in an easy to read and searchable format.</p>
+                    <p>Easily connect people, places and items together with easily created links and empower your players to interact more your world.</p>
+                    <p>Track time and progress through your campaigns adventure log and calendar features.  Countdown to catastrophies, weddings and rituals and allow your players to plan their time accordingly.</p>
                 </div>
             </div>
         </div>
         <div class="col-md-6">
-            <div class="panel panel-default top-right">
+            <div class="panel panel-default top-right home">
                 <div class="panel-heading">
-                    <h3>This site is awesome!</h3>
+                    <h3>Create a new campaign!</h3>
                 </div>
                 <div class="panel-body">
-                    <ul>
-                        <li>Store monsters, npcs, places and more!</li>
-                    </ul>
+                    <p>Campaigns serve to contain all your custom content for that adventure, making it searchable</p>
                 </div>
             </div>
         </div>
     </div>
     <div class="row">
         <div class="col-md-6">
-            <div class="panel panel-default bottom-left">
+            <div class="panel panel-default bottom-left home">
                 <div class="panel-heading">
                     <h3>This site is awesome!</h3>
                 </div>
@@ -42,7 +40,7 @@
             </div>
         </div>
         <div class="col-md-6">
-            <div class="panel panel-default bottom-right">
+            <div class="panel panel-default bottom-right home">
                 <div class="panel-heading">
                     <h3>This site is awesome!</h3>
                 </div>
